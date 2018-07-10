780f6d9cc4eba944908f1007884de83eb889fd25
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-10 00:50:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,8 +25,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(?([0-9]{3})\)?([ -]?)(?([0-9]{3})\)?([ -]?)([0-9]{4})/) == nil
-    false  
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})\)?([ -]?)([0-9]{4})/) == nil
+    false       # \(?([0-9]{3})\)?([ -]?)([0-9]{3})\)?([ -]?)([0-9]{4})
   else 
     true 
   end 
